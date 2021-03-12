37a305ef3a84c59ac96c6023120315c8b4b8484d
Mooqan <districkwar@gmail.com>
2021-03-13 03:48:19 +0600

    test commint

---
+++ index.js
@@ -0,0 +1,16 @@
+const person = new Object({
+    name: 'Mooqan',
+    age: 20,
+    greet: function() {
+        console.log('Greet');
+    }
+})
+
+Object.prototype.warning = function() {
+    console.log('Say Hello To My Little Friend!');
+}
+
+const Han = Object.create(person)
+Han.name = 'Ilgiz';
+
+const str = new String('I am Groot');
